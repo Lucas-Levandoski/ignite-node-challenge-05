0c2cca9f0f2fdaddd5ce70fcc17873c58cd12bdf
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-07-05 22:16:57 -0300

    first describe fixed

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,20 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    // return this.repository
+    //   .createQueryBuilder()
+    // Complete usando query builder
+    throw new Error('')
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(''); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    // return this.repository
+    //   .createQueryBuilder()
+    // Complete usando query builder
+    throw new Error('')
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,24 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await (await this.repository.findOneOrFail({ where: { id: user_id } }));
+
+    console.log(user.games);
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const firstName = first_name[0].toUpperCase() + first_name.slice(1).toLowerCase();
+    const lastName = last_name[0].toUpperCase() + last_name.slice(1).toLowerCase();
+
+    return this.repository.query(`SELECT * FROM users WHERE first_name LIKE '${firstName}' AND last_name LIKE '${lastName}' `);
   }
 }
